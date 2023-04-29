61e75c2490c118bfa42622858ad9e2f60c96e3f2
Nazarov, Rodion <nazarrod@fel.cvut.cz>
2023-04-29 19:34:30 +0200

    classes Hero & Enemy (not complete)

--- advgem/src/main/java/nazarrod/adventgem/advgem/model/Hero.java
+++ advgem/src/main/java/nazarrod/adventgem/advgem/model/Hero.java
@@ -6,6 +6,8 @@ public class Hero implements Serializable {
 
     private int xpos = 0;
     private int ypos = 0;
+    private final int xspeed = 0; //SHOULDN'T BE ZERO
+    private final int yspeed = 0; //SHOULDN'T BE ZERO
     private final int width = 40;
     private final int height = 60;
     private int hp = 100;
