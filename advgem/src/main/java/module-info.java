0354c2bff428f45aa8f8054671b43e29294fdbbe
Nazarov, Rodion <nazarrod@fel.cvut.cz>
2023-05-22 12:53:11 +0200

    bug fix

--- advgem/src/main/java/module-info.java
+++ advgem/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module nazarrod.adventgem.advgem {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.logging;
-    requires org.junit.jupiter.api;
+//    requires org.junit.jupiter.api;
 
     exports nazarrod.adventgem.advgem;
     opens nazarrod.adventgem.advgem to javafx.fxml;
